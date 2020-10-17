99cdd3550811011ede66946d4f0f1e9fa3549835
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-10-17 16:09:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,7 +29,6 @@ def initial_round
   first_card = deal_card
   second_card = deal_card
   card_total = first_card += second_card
-  display_card_total(card_total)
   return display_card_total(card_total)
   # code #initial_round here
 end
